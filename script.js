371072c8e85649e1ebd80cc2d0e39cd942c299a8
Pooja Jaiswal <jaiswal.pooja1809@gmail.com>
2023-09-09 22:29:10 +0530

    improve the alignment

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
+const faqItems = document.querySelectorAll('.faq-item');  
+      
+faqItems.forEach((faqItem) => {
+    const arrowIcon = faqItem.querySelector('.faq-arrow');
+    const answer = faqItem.querySelector('.faq-answer');
 
-    // Get all FAQ question elements
-    const faqItems = document.querySelectorAll('.faq-item');
-
-    // Add click event listeners to toggle answer visibility
-    faqItems.forEach((faqItem) => {
-        const arrowIcon = faqItem.querySelector('.faq-arrow');
-        const answer = faqItem.querySelector('.faq-answer');
-
-        arrowIcon.addEventListener('click', () => {
-            answer.classList.toggle('show-answer');
-        });
+    arrowIcon.addEventListener('click', () => {
+        answer.classList.toggle('show-answer');
     });
+});
 
-   
-    function toggleInfo(cardNumber) {
-        const infoCard = document.getElementById(`info-card-${cardNumber}`);
-        if (infoCard.style.display === 'none' || infoCard.style.display === '') {
-            infoCard.style.display = 'block';
-        } else {
-            infoCard.style.display = 'none';
-        }
+function toggleInfo(cardNumber) {
+    const infoCard = document.getElementById(`info-card-${cardNumber}`);
+    if (infoCard.style.display === 'none' || infoCard.style.display === '') {
+        infoCard.style.display = 'block';
+    } else {
+        infoCard.style.display = 'none';
     }
+}
+
+const toggleDarkMode = () => {
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-mode');
+};
+
+const darkModeButton = document.querySelector('#dark-mode-button');
+darkModeButton.addEventListener('click', toggleDarkMode);
+
+    
     
     
    
     
 
-   
-    // function toggleInfo(cardId) {
-    //     const card = document.getElementById(cardId);
-    //     const infoCard = card.querySelector('.info-card');
-        
-    //     if (infoCard.style.display === 'block') {
-    //         infoCard.style.display = 'none';
-    //     } else {
-    //         infoCard.style.display = 'block';
-    //     }
-    // }
-
+  
     
 
